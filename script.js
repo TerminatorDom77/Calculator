1cf9110415c65008aa552a559318a071c1dd7d01
TerminatorDom77 <dominicliechty@gmail.com>
2024-07-20 17:40:35 -0600

    Update styling of calculator. (still no functionality for parenthesis)

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let operandTwo = '';
 let operator = null;
 let operationMade = false;
 let parenthesisNum = 0;
+let lastButtonPressed = null;
 
 function operate(operandOne, operandTwo, operator){
     operandOne.includes('.') ? operandOne = parseFloat(operandOne) : operandOne = parseInt(operandOne);
@@ -35,13 +36,17 @@ for (let row = 0; row < 5; row++){
         let button = document.createElement('button');
         button.id = buttonSymbols[row][pos];
         button.textContent = button.id;
-        button.style.cssText = `flex-basis: ${96}px; font-size: ${14}pt; background-color: white;
-        border-radius: 25px; border-color: gray; border-style: outset; margin: 2px;`
+        button.style.cssText = `flex-basis: ${94}px; font-size: ${14}pt; background-color: #fffdde;
+        border-radius: 25px; border-color: none; border-style: outset; margin: 3px;`
         buttons.appendChild(button);
      
         if (numbers.includes(parseInt(button.id))){
+            button.onmousedown = () => button.style.backgroundColor = '#fcfcf2';
+            button.onmouseup = () => button.style.backgroundColor = '#fffdde';
             let text = parseInt(button.id);
             button.addEventListener('click', () => {
+                lastButtonPressed=  'number';
+
                 if (screen.textContent == ''){
                     screen.textContent = text;
                 }else {
@@ -64,17 +69,26 @@ for (let row = 0; row < 5; row++){
                 }
             });
         }else if (operators.includes(button.id)){
+            button.onmousedown = () => button.style.backgroundColor = '#ffdb6e';
+            button.onmouseup = () => button.style.backgroundColor = 'orange';
+            button.style.backgroundColor = 'orange';
             button.addEventListener('click', () => {
+                lastButtonPressed = 'operator';
+
                 if (operandOne == ''){
                     alert('Enter a number before an operator');
                 } else if(button.id != '=') {
                     screen.textContent += ' ' + button.id + ' ';
                     if (operator != '' && operandOne != '' && operandTwo != ''){
-                        screen.textContent = operate(operandOne, operandTwo, operator);
-                        operandOne = screen.textContent;
-                        operationMade = true;
-                        operandTwo = '';
-                        operator = button.id;
+                        if (parenthesisNum == 0){
+                            screen.textContent = operate(operandOne, operandTwo, operator);
+                            operandOne = screen.textContent;
+                            operationMade = true;
+                            operandTwo = '';
+                            operator = button.id;
+                        }else {
+                            
+                        }
                     }else {
                         operator = button.id;
                     }
@@ -85,26 +99,38 @@ for (let row = 0; row < 5; row++){
             });
         }else {
             button.addEventListener('click', () => {
+                button.onmousedown = () => button.style.backgroundColor = '#ffdb6e';
+                button.onmouseup = () => button.style.backgroundColor = 'orange';
                 if (button.id == 'C'){
                     screen.textContent = '';
                     operandOne = '';
                     operandTwo = '';
                     operator = null;
+                    lastButtonPressed = 'C';
                 }else if (button.id == '()'){
-                    if (parenthesisNum % 2 == 0){
-                        screen.textContent += '(';
+                    lastButtonPressed = '()'
+                    if (parenthesisNum < 4){
+                        if (parenthesisNum % 2 == 0){
+                            screen.textContent += '(';
+                        }else {
+                            screen.textContent += ')';
+                        }
+                        parenthesisNum += 1;
                     }else {
-                        screen.textContent += ')';
+                        alert("You can only have two sets of parenthesis.")
                     }
-                    parenthesisNum += 1;
                 }else if (button.id == '%'){
                     if (operator == null){
                         operandOne = parseInt(operandOne) / 100;
                         operandOne = operandOne.toString();
                         screen.textContent = operandOne;
+                        lastButtonPressed = '%';
                     }
                 }else if (button.id == 'x'){
                     screen.textContent = screen.textContent.slice(0, -1);
+                    if (lastButtonPressed == '()'){
+                        parenthesisNum -= 1;
+                    }
                 } else {
                     screen.textContent += '.';
                     if (operandOne != '' && operandTwo == ''){
